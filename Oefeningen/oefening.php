0fc6ea0faa2af74a9f8fa5dc3ebf444d35b09736
Chahin <chahin.aarbiou@edu-kw1c.nl>
2020-02-12 09:24:53 +0100

    First commit

---
+++ Oefeningen/oefening.php
@@ -0,0 +1,29 @@
+<?php
+$car = "audi";
+$license = true;
+$age = 24;
+$price = 200.9;
+
+echo $car . " heeft een prijs ". $price;
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>
+        Opdracht 13.3
+    </title>
+</head>
+<body>
+        <h1>
+            $car = "audi";   string <br>
+            $license = true;    boolean <br>
+            $age = 24   integer <br>
+            #price = 2003.33; float
+        </h1>
+</body>
+</html>
+
---
+++ Template/index.php
@@ -0,0 +1,54 @@
+<?php
+/*
+*  User:       Chahin Aarbiou
+*  Date:       12-2-2020
+*  Time:       08:56
+*  File:       index.php
+ * */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--        tekenset declareren-->
+        <meta chrset="utf-8">
+        <!--        omschrijving van de webpagina-->
+        <meta name="description" content="">
+        <!--        Toekennen van sleutelwoorden-->
+        <meta name="keywords" content="">
+        <!--        het html bestandje verbinden met het css bestandje-->
+        <link href="css/style.css" type="text/css" rel="stylesheet">
+        <title>
+            Template
+        </title>
+    </head>
+    <body>
+        <header>
+            <h1>
+                <?php echo "Uitwerking van PHP-opdrachten"; ?>
+            </h1>
+        </header>
+        <aside>
+            <h2>
+                Menu
+            </h2>
+            <ul>
+                <li>
+                    Hoofdstuk 2
+                    <ul>
+                        <li>
+                            <a href="">Opdracht 2.1</a>
+                        </li>
+                        <li>
+                            <a href="">Opdracht 2.2</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </aside>
+        <main id="wrapper">
+            <h2>
+                Uitwerkingen
+            </h2>
+        </main>
+    </body>
+    </html>
