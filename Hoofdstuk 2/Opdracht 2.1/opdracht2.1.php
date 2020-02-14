25fb922744238c6b44e04ac01bc01a0268a7e125
Chahin <chahin.aarbiou@edu-kw1c.nl>
2020-02-14 13:43:13 +0100

    H2.1 invoegen.

--- Hoofdstuk 2/Opdracht 2.1/opdracht2.1.php
+++ Hoofdstuk 2/Opdracht 2.1/opdracht2.1.php
@@ -1,67 +1,44 @@
 <?php
 /*
 *  User:       Chahin Aarbiou
-*  Date:       12-2-2020
+*  Date:       14-2-2020
 *  Time:       08:56
-*  File:       index.php
+*  File:       opdracht2.1.php
  * */
 ?>
 <!DOCTYPE html>
-<html>
+
+<html lang="en">
+<!-- start van de head -->
 <head>
-    <!--        tekenset declareren-->
-    <meta chrset="utf-8">
-    <!--        omschrijving van de webpagina-->
-    <meta name="description" content="">
-    <!--        Toekennen van sleutelwoorden-->
-    <meta name="keywords" content="">
-    <!--        het html bestandje verbinden met het css bestandje-->
-    <link href="css/style.css" type="text/css" rel="stylesheet">
+    <!-- declaratie van de tekenset -->
+    <meta charset="utf-8">
+    <!-- omschrijving van de website -->
+    <meta name="description" content="opdracht A">
+    <!-- keywords van de website -->
+    <meta name="keywords" content="keyword 1, keyword 2">
+    <!-- koppeling naar CSS bestand -->
+    <link type="text/css" rel="stylesheet" href="Style.css">
     <title>
-        index
+        Template
     </title>
 </head>
 <body>
-<header>
-    <h1>
-        <?php echo "Uitwerking van PHP-opdrachten"; ?>
-    </h1>
-</header>
-<aside>
-    <h2>
-        Menu
-    </h2>
-    <ul>
-        <li>
-            Hoofdstuk 2
-            <ul>
-                <li>
-                    <a href="">Opdracht 2.1</a>
-                </li>
-                <li>
-                    <a href="">Opdracht 2.2</a>
-                </li>
-            </ul>
-        </li>
-    </ul>
-    <ul>
-        <li>
-            Hoofdstuk 2
-            <ul>
-                <li>
-                    <a href="">Opdracht 2.1</a>
-                </li>
-                <li>
-                    <a href="">Opdracht 2.2</a>
-                </li>
-            </ul>
-        </li>
-    </ul>
-</aside>
-<main id="wrapper">
-    <h2>
-        Uitwerkingen
-    </h2>
-</main>
-</body>
+    <header>
+        <h1><?php echo "IO1A4 Opdracht 2.1"; ?></h1>
+    </header>
+    <p>
+        <?php  echo "Hello world!"; ?>
+    </p>
+    <p>
+        <a href="../../index.php">Terug</a>
+    </p>
+
+
+
+
+
+
+
+    </body>
 </html>
