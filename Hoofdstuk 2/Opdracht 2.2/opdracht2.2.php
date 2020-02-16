94a9a786c16ceafb7dd2615e4fc09245026fc3d1
Chahin <chahin.aarbiou@edu-kw1c.nl>
2020-02-16 17:08:09 +0100

    Uiteindelijk alles klaar met commentaar

--- Hoofdstuk 2/Opdracht 2.2/opdracht2.2.php
+++ Hoofdstuk 2/Opdracht 2.2/opdracht2.2.php
@@ -1,9 +1,26 @@
 <?php
+$text1 = "Hallo";
+$text2 = "een makkelijke taal";
+$text3 = "toch zo'n makkelijke taal";
+$text4 = "wat is";
+$text5 = "PHP";
+$text6 = "Nooit gedacht dat ";
+$text7 = "De installatie is best ingewikkeld";
+$text8 = "Fijn";
+$text9 = "?";
+$text10 = ".";
+$text11 = ",";
+$text12 = "<br>";
+$text13 = "is";
+$text14 = "Vind je niet";
+$text15 = "toch";
+?>
+<?php
 /*
 *  User:       Chahin Aarbiou
 *  Date:       16-2-2020
-*  Time:        16:31
-*  File:       opdracht2.1.php
+*  Time:       16:34
+*  File:       opdracht2.2.php
  * */
 ?>
 <!DOCTYPE html>
@@ -19,15 +36,14 @@
         <meta name="keywords" content="keyword 1, keyword 2">
         <!-- koppeling naar CSS bestand -->
         <link type="text/css" rel="stylesheet" href="style.css">
-    <!--  Titel dat bovenaan website komt.  -->
         <title>
-            Opdracht 2.1
+            Opdracht 2.2
         </title>
     </head>
     <body>
         <!--  Hier begint een header die bovenaan website komt te staan.  -->
         <header>
-            <h1><?php echo "Opdracht 2.1"; ?></h1>
+            <h1><?php echo "Opdracht 2.2"; ?></h1>
         </header>
         <!--  De menu.  -->
         <aside id="menu">
@@ -40,11 +56,11 @@
                     <ul>
                         <!--  Een link naar de index  -->
                         <li>
-                            <a href="../../index.php">Terug naar menu</a>
+                            <a href="../Opdracht%202.1/opdracht2.1.php">Opdracht 2.1</a>
                         </li>
-                        <!--  Een link naar de bestand  -->
+                        <!--  Een link naar de index  -->
                         <li>
-                            <a href="../Opdracht%202.2/opdracht2.2.php">Opdracht 2.2</a>
+                            <a href="../../index.php">Terug naar menu</a>
                         </li>
                     </ul>
                 </li>
@@ -115,14 +131,38 @@
             <!--  Een koptekst 2  -->
             <h2>Uitwerkingen</h2>
             <br>
-            <!--  Tekst inplaatsen met behulp van een echo [PHP].  -->
-            <p>
-                <?php echo "Hello world!"; ?>
-            </p>
-            <!--  Een link die je brengt naar de index.  -->
-            <p>
-                <a href="../../index.php">Terug</a>
-            </p>
+            <!--  Een aside zodat alles wat hieronder goed in CSS kan opmaken.  -->
+            <aside id="big">
+                <!--  Een koptekst 2  -->
+                <h2>
+                    Taak 1
+                </h2>
+                <!--  Een aside met daarin de zin.  -->
+                <aside class="grey">
+                    <!--  Een koptekst 2  -->
+                    <p>
+                        <?php
+                        echo    $text1 . $text11 . " " . $text4 . " " . $text5 . " " . $text3 . $text10 . $text12
+                            . $text7 . $text10 . " " . $text8 . " " . $text15 . $text9 . $text12 . $text6 . " " . $text5 . " " .
+                            $text3 . " " . $text13 . $text10;
+                        ?>
+                    </p>
+                </aside>
+                <br>
+                <!--  Een koptekst 2  -->
+                <h2>
+                    Taak 2
+                </h2>
+                <!--  Een aside met daarin de zin.  -->
+                <aside class="grey">
+                    <p>
+                        <?php
+                        echo    $text1 . $text11 . $text12 . $text8 . " " . $text15 . " dat " . $text5 . " "
+                            . $text3 . $text10 . $text12 . $text7 . $text10 . " " . $text14 . $text9
+                        ?>
+                    </p>
+                </aside>
+            </aside>
         </main>
     </body>
 </html>
