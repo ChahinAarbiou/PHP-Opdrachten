72996d6ac3ab85dbe9e849331f847bd697ba3b88
Chahin <chahin.aarbiou@edu-kw1c.nl>
2020-02-16 16:24:31 +0100

    Hoofdstuk 2 uploaden

--- Hoofdstuk 2/Opdracht 2.1/opdracht2.1.php
+++ Hoofdstuk 2/Opdracht 2.1/opdracht2.1.php
@@ -1,26 +1,9 @@
 <?php
-$text1 = "Hallo";
-$text2 = "een makkelijke taal";
-$text3 = "toch zo'n makkelijke taal";
-$text4 = "wat is";
-$text5 = "PHP";
-$text6 = "Nooit gedacht dat ";
-$text7 = "De installatie is best ingewikkeld";
-$text8 = "Fijn";
-$text9 = "?";
-$text10 = ".";
-$text11 = ",";
-$text12 = "<br>";
-$text13 = "is";
-$text14 = "Vind je niet";
-$text15 = "toch";
-?>
-<?php
 /*
 *  User:       Chahin Aarbiou
 *  Date:       12-2-2020
 *  Time:       08:56
-*  File:       index.php
+*  File:       opdracht2.1.php
  * */
 ?>
 <!DOCTYPE html>
@@ -37,12 +20,12 @@ $text15 = "toch";
     <!-- koppeling naar CSS bestand -->
     <link type="text/css" rel="stylesheet" href="style.css">
     <title>
-        Opdracht 2.2
+        Opdracht 2.1
     </title>
 </head>
 <body>
 <header>
-    <h1><?php echo "Opdracht 2.2"; ?></h1>
+    <h1><?php echo "Opdracht 2.1"; ?></h1>
 </header>
 <aside id="menu">
     <h2>Menu</h2>
@@ -50,10 +33,10 @@ $text15 = "toch";
         <li>Hoofdstuk 2
             <ul>
                 <li>
-                    <a href="../Opdracht%202.1/opdracht2.1.php">Opdracht 2.1</a>
+                    <a href="../../index.php">Terug naar menu</a>
                 </li>
                 <li>
-                    <a href="../../index.php">Terug naar menu</a>
+                    <a href="../Opdracht%202.2/opdracht2.2.php">Opdracht 2.2</a>
                 </li>
             </ul>
         </li>
@@ -107,32 +90,12 @@ $text15 = "toch";
 <main id="wrapper">
     <h2>Uitwerkingen</h2>
     <br>
-    <aside id="big">
-        <h2>
-            Taak 1
-        </h2>
-        <aside class="grey">
-            <p>
-                <?php
-                echo    $text1 . $text11 . " " . $text4 . " " . $text5 . " " . $text3 . $text10 . $text12
-                    . $text7 . $text10 . " " . $text8 . " " . $text15 . $text9 . $text12 . $text6 . " " . $text5 . " " .
-                    $text3 . " " . $text13 . $text10;
-                ?>
-            </p>
-        </aside>
-        <br>
-        <h2>
-            Taak 2
-        </h2>
-        <aside class="grey">
-            <p>
-                <?php
-                echo    $text1 . $text11 . $text12 . $text8 . " " . $text15 . " dat " . $text5 . " "
-                    . $text3 . $text10 . $text12 . $text7 . $text10 . " " . $text14 . $text9
-                ?>
-            </p>
-        </aside>
-    </aside>
+    <p>
+        <?php  echo "Hello world!"; ?>
+    </p>
+    <p>
+        <a href="../../index.php">Terug</a>
+    </p>
 </main>
 </body>
 </html>
