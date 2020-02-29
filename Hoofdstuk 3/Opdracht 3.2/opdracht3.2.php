7aff290b65c12331aab8ff01090d8fb18385284a
Chahin <chahin.aarbiou@edu-kw1c.nl>
2020-02-29 01:26:22 +0100

    Opdracht 3.2 helemaal klaar

--- Hoofdstuk 3/Opdracht 3.2/opdracht3.2.php
+++ Hoofdstuk 3/Opdracht 3.2/opdracht3.2.php
@@ -33,6 +33,36 @@ include "../../includes/menu.php";
             echo "<p id='red'>" . "U moet stoppen!" . "</p>";
         }
     ?>
+    <br>
+    <!-- Een paragraaf   -->
+    <p>Taak 2</p>
+    <?php
+        $countryName = "Nederland"; // Een string variabel
+        $currentAge = 17; // Een integer variabel
+
+        // In de pagina een tekst invoegen met je waarde van de bovenstaande variabelen.
+        echo "Je woont in " . $countryName . " en je bent " . $currentAge . " jaar oud. <br>";
+
+        /* Als België de land is en je bent 18 of ouder runt die onderstaande if uit.
+           Als Bulgarije of Nederland de land is en je bent 18 of ouder runt die onderstaande if uit.
+           Als Cyprus de land is en je bent 17 of ouder runt die onderstaande if uit.
+           Als Zweden de land is en je bent 20 of ouder runt die onderstaande if uit.  */
+        if (($countryName == "België" and $currentAge >= 18) or (($countryName == "Bulgarije" or $countryName == "Nederland") and $currentAge >= 18 ) or ($countryName == "Cyprus" and $currentAge >= 17) or ($countryName == "Zweden" and $currentAge >= 20))
+        {
+            echo "Je mag hier alle alcohol drinken."; // Vult de pagina de tekst die tussen de dubbele aanhalingstekens staan.
+        }
+        /* Als België de land is en je bent 16 of ouder runt die onderstaande if uit.
+           Als Zweden de land is en je bent 18 of ouder runt die onderstaande if uit. */
+        else if (($countryName == "België" and $currentAge >= 16) or ($countryName == "Zweden" and $currentAge >= 18))
+        {
+            echo "Je mag hier alleen zwakke alcohol drinken."; // Vult de pagina de tekst die tussen de dubbele aanhalingstekens staan.
+        }
+        /* Als de bovenste if of else if false zijn wordt de onderstaande else uitgevoerd. */
+        else
+        {
+            echo "Je mag hier geen alcohol drinken."; // Vult de pagina de tekst die tussen de dubbele aanhalingstekens staan.
+        }
+    ?>
 </main>
 </body>
 </html>
