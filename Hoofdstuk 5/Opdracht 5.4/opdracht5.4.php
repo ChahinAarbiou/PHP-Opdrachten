2eb275e6db48d4ed3e0bd58cf9ac25737bdc2110
Chahin <chahin.aarbiou@edu-kw1c.nl>
2020-04-11 16:37:25 +0200

    Oplevering opdracht 5.4

--- Hoofdstuk 5/Opdracht 5.4/opdracht5.4.php
+++ Hoofdstuk 5/Opdracht 5.4/opdracht5.4.php
@@ -14,6 +14,99 @@ include "../../includes/footer.php";
     <h2>
         Uitwerkingen
     </h2>
+    <!-- Een formulier starten die de gegevens naar zichzelf stuurt (action). Dit doet die met een post method. -->
+    <form action="opdracht5.4.php" method="post">
+        <br>
+        <!-- Koptekst 3 -->
+        <h3>
+            Stoplicht
+        </h3>
+        <!-- Een table -->
+        <table>
+            <tr>
+                <!-- Een breedte geven zodat alles goed bij elkaar staat.-->
+                <td width="250px">
+                    Komt er een ambulance aan?
+                </td>
+                <td>
+                    <!-- Een radiobutton met een value "ja".-->
+                    <input name="ambulanceComing" type="radio" value="ja">
+                        Ja
+                    </input>
+                    <!-- Een radiobutton met een value "nee".-->
+                    <input name="ambulanceComing" type="radio" value="nee">
+                        Nee
+                    </input>
+                </td>
+            </tr>
+            <tr>
+                <!-- Een cel-->
+                <td>
+                    Stoplicht kleur ?
+                </td>
+                <td>
+                    <!-- Een radiobutton met een value "rood".-->
+                    <input name="trafficLightColor" type="radio" value="rood">
+                        Rood
+                    </input>
+                    <!-- Een radiobutton met een value "groen".-->
+                    <input name="trafficLightColor" type="radio" value="groen">
+                        Groen
+                    </input>
+                    <!-- Een radiobutton met een value "oranje".-->
+                    <input name="trafficLightColor" type="radio" value="oranje">
+                        Oranje
+                    </input>
+                </td>
+            </tr>
+            <tr>
+                <!-- Een cel-->
+                <td>
+                    <!-- Een submit button, bij het klikken worden de gegevens verstuurd naar zichzelf. -->
+                    <input type="submit" name="submit">
+                </td>
+            </tr>
+            <tr>
+                <!-- Een colspan zodat de onderstaande cel een breedte krijgt van 2 cellen. -->
+                <td colspan="2">
+                    <br>
+                    <?php
+                        // Zodra er op de submit wordt gedrukt wordt de onderstaande if-statement gerunt.
+                        if (isset($_POST['submit'])) {
+                            // Een koptekst 3 tekst in de pagina zetten met echo.
+                            echo "<h3>Wat is de situatie en wat moet ik doen?</h3><br>";
+                            /* Als er niet wordt aangeklikt bij stoplichtkleur of ambulance dan runt die de onderstaande if-statement uit.*/
+                            if (!isset($_POST['trafficLightColor']) || !isset($_POST['ambulanceComing'])) {
+                                // Een paragraaf tekst in de pagina zetten met echo.
+                                echo "<p>Of stoplichtkleur is onbekend of het is onbekend of de ambulance komt.</p>";
+                            }
+                            //Als de if-statement false is dan runt die de onderstaande else-statement.
+                            else
+                            {
+                                // Declaren van variabelen van de post variabels.
+                                $trafficLightColor = $_POST['trafficLightColor'];
+                                $ambulanceComing = $_POST['ambulanceComing'];
+
+                                // In pagina: Stoplicht staat op [KLEUR] en er komt: [JA/NEE] een ambulance aan.
+                                echo "Stoplicht staat op " . $trafficLightColor . " en er komt: " . $ambulanceComing . " een ambulance aan.";
+
+                                // Als stoplicht kleur op groen staat en er komt geen ambulance aan dan voert die het onderstaande if-statement uit.
+                                if ($trafficLightColor == "groen" && $ambulanceComing == "nee") {
+                                    // Een paragraaf tekst in de pagina zetten met echo. Ook een id zodat ik die een kleur kan geven met CSS.
+                                    echo "<p id='green'>U mag doorrijden</p>";
+                                 }
+                                // Als stoplicht niet op groen staat en komt wel een ambulance aan dan voert die de onderstaande else-statement uit.
+                                else {
+                                    // Een paragraaf tekst in de pagina zetten met echo. Ook een id zodat ik die een kleur kan geven met CSS.
+                                    echo "<p id='red'>U moet stoppen</p>";
+                                }
+                            }
+                        }
+                    ?>
+                </td>
+            </tr>
+        </table>
+    </form>
 </main>
 </body>
 </html>
